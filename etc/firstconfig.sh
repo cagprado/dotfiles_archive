7db6d51b634841176982bbd5d018439369c916bc
Caio Prado <cagprado@yandex.com>
2017-01-03 20:43:43 -0200

    [GIT] Add hooks scripts

--- etc/firstconfig.sh
+++ etc/firstconfig.sh
@@ -3,13 +3,6 @@
 # Set repository local option
 git --git-dir=$HOME/.cfg --work-tree=$HOME config --local status.showUntrackedFiles no
 
-# Set correct permissions for some sensitive config files
-chmod 600 $HOME/.ssh/config
-chmod 600 $HOME/.gnupg/dirmngr.conf
-chmod 600 $HOME/.gnupg/gpg.conf
-chmod 600 $HOME/.gnupg/gpg-agent.conf
-chmod 600 $HOME/.msmtprc
-
 # Prevent dropbox from updating itself (pacman will do it ok)
 install -dm0 $HOME/.dropbox-dist
 
@@ -24,3 +17,10 @@ certutil -d sql:$HOME/.pki/nssdb -A -n "CCIFUSP CA" -t CT,C,C -i "$HOME/etc/cert
 certutil -d sql:$HOME/.pki/nssdb -A -n "CERN Root CA" -t ,, -i "$HOME/etc/certificates/CERN Root Certification Authority 2.crt"
 certutil -d sql:$HOME/.pki/nssdb -A -n "CERN Grid CA" -t CT,C,C -i "$HOME/etc/certificates/CERN Grid Certification Authority.crt"
 certutil -d sql:$HOME/.pki/nssdb -A -n "CERN CA" -t CT,C,C -i "$HOME/etc/certificates/CERN Certification Authority.crt"
+
+# Setup hooks
+mkdir $HOME/.cfg/hooks/scripts
+cp $HOME/etc/setpermissions.sh $HOME/.cfg/hooks/scripts
+cp $HOME/etc/post-checkout $HOME/.cfg/hooks/scripts
+cp $HOME/etc/post-merge $HOME/.cfg/hooks/scripts
+print -P "%BPlease manually setup the hooks directory.%b"
---
+++ etc/setpermissions.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+
+# Set correct permissions for some sensitive config files
+chmod 600 $HOME/.ssh/config
+chmod 600 $HOME/.gnupg/dirmngr.conf
+chmod 600 $HOME/.gnupg/gpg.conf
+chmod 600 $HOME/.gnupg/gpg-agent.conf
+chmod 600 $HOME/.msmtprc
