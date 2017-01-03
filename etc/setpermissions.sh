b898e930028d047a01ad3cff3bef2c3d06f6261a
Caio Prado <cagprado@yandex.com>
2017-01-03 21:13:17 -0200

    [permissions] Add permission to .gnupg dir

--- etc/setpermissions.sh
+++ etc/setpermissions.sh
@@ -5,4 +5,5 @@ chmod 600 $HOME/.ssh/config
 chmod 600 $HOME/.gnupg/dirmngr.conf
 chmod 600 $HOME/.gnupg/gpg.conf
 chmod 600 $HOME/.gnupg/gpg-agent.conf
+chmod 700 $HOME/.gnupg
 chmod 600 $HOME/.msmtprc
