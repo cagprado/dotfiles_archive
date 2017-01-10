3764f531557013ff4c356e9829d151bb17fb2425
Caio Prado <cagprado@yandex.com>
2017-01-10 15:54:17 -0200

    [bin] Remove notmuch scripts (they are now aliases).

--- bin/notmuch_retrieve.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-[[ "$1" == "-l" ]] && local MREDSON=mredson || :
-
-# Dump notmuch database on server (mredson) and download it
-FILENAME="/tmp/$$.$(date +%Y%m%d%H%M).notmuchdump"
-ssh cagprado@$MREDSON "notmuch dump| xz" | xz -d | notmuch restore
--- bin/notmuch_send.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-[[ "$1" == "-l" ]] && local MREDSON=mredson || :
-
-# Dump notmuch database here and retrieve it on server (mredson)
-notmuch dump | xz | ssh cagprado@$MREDSON "xz -d | notmuch restore" 
