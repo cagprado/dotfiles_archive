ba0f0156fbd287ee97c740fac0ca01a00d18808e
Caio Prado <cagprado@yandex.com>
2017-07-05 11:46:33 -0300

    Add avatar to SDDM

--- etc/setup/setpermissions.sh
+++ etc/setup/setpermissions.sh
@@ -7,3 +7,7 @@ chmod 600 $HOME/.gnupg/gpg.conf
 chmod 600 $HOME/.gnupg/gpg-agent.conf
 chmod 700 $HOME/.gnupg
 chmod 600 $HOME/.msmtprc
+
+# Allow sddm user to read this file for avatar
+setfacl -m "u:sddm:x" /home/cagprado
+chmod 664 $HOME/.face.icon
