bd0b6021e910bda64ed7febf59ef2ed9c981e848
Caio Prado <cagprado@yandex.com>
2017-08-02 23:03:55 -0300

    [mredson] Update mredson address

--- .config/systemd/user/scripts/dns-update.zsh
+++ .config/systemd/user/scripts/dns-update.zsh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 # This script is to be used in systemd.
 # Updates freedns.
-wget -qO- --read-timeout=0.0 --waitretry=5 --tries=400 'https://freedns.afraid.org/dynamic/update.php?S1A4ek1UVnRldDhvSU1SMHI2VXg6MTY0MzY3ODU=' > /dev/null
+
+URL='https://freedns.afraid.org/dynamic/update.php?S1A4ek1UVnRldDhvSU1SMHI2VXg6MTUyMTQ3MDM='
+wget -qO- --read-timeout=0.0 --waitretry=5 --tries=400 $URL > /dev/null
