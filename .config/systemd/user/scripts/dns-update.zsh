902076e1749f20276151bbe163eddde824f4ca42
Caio Prado <cagprado@yandex.com>
2017-09-13 20:04:40 +0800

    [dns] Update url as per password change

--- .config/systemd/user/scripts/dns-update.zsh
+++ .config/systemd/user/scripts/dns-update.zsh
@@ -2,5 +2,5 @@
 # This script is to be used in systemd.
 # Updates freedns.
 
-URL='https://freedns.afraid.org/dynamic/update.php?S1A4ek1UVnRldDhvSU1SMHI2VXg6MTUyMTQ3MDM='
+URL='https://freedns.afraid.org/dynamic/update.php?MU5pM1hKdHd4N2lUSUg3aWhCaVY6MTcwNDcwOTU='
 wget -qO- --read-timeout=0.0 --waitretry=5 --tries=400 $URL > /dev/null
