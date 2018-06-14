bd75301e265b471dea3fd820bbf77d77ce503024
Caio Prado <cagprado@yandex.com>
2018-06-14 21:17:22 +0800

    [src] Update neovim source

--- src/neovim/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git clone 'https://github.com/neovim/neovim.git'
-cd neovim
-
-make -j CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/usr/local/neovim/" CMAKE_BUILD_TYPE=RelWithDebInfo
-make install
