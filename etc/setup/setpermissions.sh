ac1294985adbf0d084effd7209a23e7107a7882c
Caio Prado <cagprado@yandex.com>
2018-08-13 20:01:31 +0800

    [setpermissions] Fix filename

--- etc/setup/setpermissions.sh
+++ etc/setup/setpermissions.sh
@@ -7,7 +7,7 @@ chmod 600 $HOME/.gnupg/gpg.conf
 chmod 600 $HOME/.gnupg/gpg-agent.conf
 chmod 700 $HOME/.gnupg
 chmod 600 $HOME/.msmtprc
-chmod 755 $HOME/system/etc/NetworkManager/dispatcher.d/99-disable-wifi-on-wired
+chmod 755 $HOME/etc/system/etc/NetworkManager/dispatcher.d/99-disable-wifi-on-wired
 
 # Allow sddm user to read this file for avatar
 chmod 664 $HOME/.face.icon
