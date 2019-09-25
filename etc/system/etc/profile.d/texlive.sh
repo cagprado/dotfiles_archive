59ce0b9e2882fc37bd77d56841775f13a6508940
Caio Prado <cagprado@yandex.com>
2019-09-25 08:56:46 +0800

    [texlive] Move configuration to the global scope

---
+++ etc/system/etc/profile.d/texlive.sh
@@ -0,0 +1,3 @@
+export PATH=/usr/local/texlive/bin/x86_64-linux:${PATH}
+#export MANPATH=/usr/local/texlive/texmf-dist/doc/man:${MANPATH}
+#export INFOPATH=/usr/local/texlive/texmf-dist/doc/info:${INFOPATH}
