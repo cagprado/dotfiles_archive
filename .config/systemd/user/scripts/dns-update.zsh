2939a17b3a4003d0cdefd0f4d492dca28612b9a9
Caio Prado <cagprado@yandex.com>
2020-04-20 14:42:32 +0800

    [systemd] Remove unnecessary script

--- .config/systemd/user/scripts/dns-update.zsh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-# This script is to be used in systemd.
-# Updates freedns.
-
-URL='https://freedns.afraid.org/dynamic/update.php?MU5pM1hKdHd4N2lUSUg3aWhCaVY6MTcwNDcwOTU='
-wget -qO- --read-timeout=0.0 --waitretry=5 --tries=400 $URL > /dev/null
