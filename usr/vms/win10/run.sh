7b18ca57cb874d9447892b787ec4ab9dafcc51b7
Caio Prado <cagprado@yandex.com>
2020-08-07 16:02:49 +0800

    [vm] Review script and unify qemu options

--- usr/vms/win10/run.sh
+++ usr/vms/win10/run.sh
@@ -9,66 +9,63 @@
 # Download virtio windows driver (before install):
 # https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
 
-# make sure HiDPI settings won't mess with guest system
+## SETUP ENVIRONMENT ########################################################
+# disable HiDPI settings (SPICE won’t scale properly otherwise)
 export GDK_SCALE=1
 export GDK_DPI_SCALE=1
 
-# prepare virtual GPU (intel GVT)
+# virtual GPU (intel GVT)
 export MESA_LOADER_DRIVER_OVERRIDE=i965
 GVT_PCI="/sys/bus/pci/devices/0000:00:02.0"
 GVT_TYPE="i915-GVTg_V5_4"
 GVT_GUID="b1306297-6c1e-404f-81e8-b43aab0aa3b0"
 
-echo "${GVT_GUID}" | sudo tee ${GVT_PCI}/mdev_supported_types/${GVT_TYPE}/create > /dev/null
-sudo setfacl -m u:cagprado:rw /dev/vfio/18 \
-    && sleep 5 || exit 1
+## SETUP VIRTUAL MACHINE ####################################################
+# basic QEMU command
+COMMAND="qemu-system-x86_64 -display spice-app,gl=on"
+# CPU/KVM
+COMMAND+="    -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time"
+COMMAND+="    -enable-kvm -machine q35 -device intel-iommu,caching-mode=on"
+COMMAND+="    -smp 4,cores=2,threads=2"
+# memory
+COMMAND+="    -m $(echo "$(awk '/MemTotal/{print $2}' /proc/meminfo)/2000" | bc)M"
+# USB/Network
+COMMAND+="    -usb -device usb-tablet -soundhw hda"
+COMMAND+="    -nic user,model=virtio-net-pci,smb=$HOME/var/shared"
+# GPU
+COMMAND+="    -vga none"
+COMMAND+="    -device vfio-pci,sysfsdev=/sys/bus/mdev/devices/$GVT_GUID,"
+COMMAND+="display=on,x-igd-opregion=on,ramfb=on,driver=vfio-pci-nohotplug"
+# SPICE
+COMMAND+="    -device virtio-serial-pci"
+COMMAND+="    -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0"
+COMMAND+="    -chardev spicevmc,id=spicechannel0,name=vdagent"
+COMMAND+="    -spice unix,addr=/tmp/vm_spice.socket,disable-ticketing"
+# DISKS
+COMMAND+="    -drive file=hdd.qcow2,index=0,media=disk,format=qcow2,if=virtio"
 
-
-if [[ "$HOSTNAME" = "mredson" ]]; then
-    export OPTIONS="-smp 4,cores=2,threads=2 -m 8G"
-else
-    export OPTIONS="-smp 4,cores=2,threads=2 -m 4G"
+# create HD if not present
+if [[ ! -f hdd.qcow2 ]]; then
+    if [[ -f hdd0.qcow2 ]]; then
+        # create disk from overlay
+        qemu-img create -o backing_file=hdd0.qcow2,backing_fmt=qcow2 -f qcow2 hdd.qcow2
+    else
+        # create empty disk and prepare installation
+        COMMAND+=" -drive file=win10.iso,index=2,media=cdrom"
+        COMMAND+=" -drive file=virtio.iso,index=3,media=cdrom"
+        COMMAND+=" -boot order=d"
+    fi
 fi
 
-# load VM
-if [[ -f hdd.qcow2 ]]; then
-    qemu-system-x86_64 -display spice-app,gl=on \
-        -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
-        -enable-kvm -machine q35 -device intel-iommu,caching-mode=on $OPTIONS \
-        -usb -device usb-tablet -soundhw hda \
-        -nic user,model=virtio-net-pci,smb=$HOME/var/shared \
-        -vga none \
-        -device vfio-pci,sysfsdev=/sys/bus/mdev/devices/$GVT_GUID,display=on,x-igd-opregion=on,ramfb=on,driver=vfio-pci-nohotplug \
-        -device virtio-serial-pci \
-        -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
-        -chardev spicevmc,id=spicechannel0,name=vdagent \
-        -spice unix,addr=/tmp/vm_spice.socket,disable-ticketing \
-        -drive file=hdd.qcow2,index=0,media=disk,format=qcow2,if=virtio $@
+## EXECUTE VM ###############################################################
 
-# create overlay image
-elif [[ -f hdd0.qcow2 ]]; then
-    qemu-img create -o backing_file=hdd0.qcow2,backing_fmt=qcow2 -f qcow2 hdd.qcow2
+function sudowrite() { echo "$1" | sudo tee "$2" >/dev/null; }
+sudowrite "${GVT_GUID}" "${GVT_PCI}/mdev_supported_types/${GVT_TYPE}/create"
 
-# install VM
-else
-    # create drive
-    qemu-img create -f qcow2 hdd.qcow2 80G
-
-    # load boot disk to install windows
-    qemu-system-x86_64 -display spice-app -boot order=d \
-        -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time \
-        -enable-kvm -machine q35 -device intel-iommu $OPTIONS \
-        -usb -device usb-tablet -soundhw hda \
-        -nic user,model=virtio-net-pci,smb=$HOME/var/shared \
-        -vga none -device qxl-vga,vram_size=67108864,vgamem_mb=64 \
-        -device virtio-serial-pci \
-        -device virtserialport,chardev=spicechannel0,name=com.redhat.spice.0 \
-        -chardev spicevmc,id=spicechannel0,name=vdagent \
-        -spice unix,addr=/tmp/vm_spice.socket,disable-ticketing \
-        -drive file=hdd.qcow2,index=0,media=disk,format=qcow2,if=virtio \
-        -drive file=win10.iso,index=2,media=cdrom \
-        -drive file=virtio.iso,index=3,media=cdrom
+if [[ -d "${GVT_PCI}/${GVT_GUID}/iommu_group" ]]; then
+    DEVICE="/dev/vfio/$(realpath ${GVT_PCI}/${GVT_GUID}/iommu_group | sed 's/.*\///')"
+    echo -n "Waiting for GPU..." && sleep 5 && echo " OK"
+    sudo setfacl -m u:cagprado:rw $DEVICE
+    $COMMAND $@
+    sudowrite 1 "${GVT_PCI}/${GVT_GUID}/remove"
 fi
-
-# remove GVT device
-echo 1 | sudo tee ${GVT_PCI}/${GVT_GUID}/remove > /dev/null
