2515569cc60f32b72e6224876bee92d72d61b257
Caio Prado <cagprado@yandex.com>
2021-07-20 22:19:25 +0800

    [contacts] Configure xandikos and vdirsyncer

--- .config/systemd/user/scripts/network-services.zsh
+++ .config/systemd/user/scripts/network-services.zsh
@@ -6,6 +6,7 @@ while ! $(wget -q --spider ping.archlinux.org); do sleep 5s; done
 # and starts network related services upon connection confirmed
 systemctl --user start mail.timer
 
-if [[ "$HOSTNAME" = "mredson" ]]; then
+if [[ "$(hostname)" = "mredson" ]]; then
   systemctl --user start xandikos.service
+  systemctl --user start vdirsyncer.timer
 fi
